3d37cdb66065811e65e118451d0f511e977603bb
Stepan Pan <stepanpan8@gmail.com>
2023-05-30 22:00:20 +0300

    deleted trash and added header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>
+      <div id="image"><img src="burlamask.png" width="125vn" height="125vn"/></div>
+      <h1>Burlamask</h1>
+      <form>
+        <input type="text"/>
+      </form>
+    </header>
     </div>
   );
 }
