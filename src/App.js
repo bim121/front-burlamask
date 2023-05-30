08a6a2b68421da7c3330163e346bf0a116b70efb
Stepan Pan <stepanpan8@gmail.com>
2023-05-30 23:31:11 +0300

    выровняный хедер и удалено лишнее опять

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ function App() {
   return (
     <div className="App">
       <header>
-      <div id="image"><img src="burlamask.png" width="125vn" height="125vn"/></div>
-      <h1>Burlamask</h1>
-      <form>
-        <input type="text"/>
-      </form>
+      <div className="wrap">
+        <img  className = "burlaMaskImg" src="burlamask.png" width="120vh" height="120vh"/>
+        <h1>BurlaMask</h1>
+      </div>
+      <div className="inputForm">
+        <form>
+          <input type="text"/>
+        </form>
+      </div>
     </header>
     </div>
   );
