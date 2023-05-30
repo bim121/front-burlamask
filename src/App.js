133bbb2f56140f45e2e7329c39117164004b2252
Stepan Pan <stepanpan8@gmail.com>
2023-05-31 01:37:06 +0300

    added footer and 1 funny picture

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header>
-      <div className="wrap">
+      <div className="wrapHeader">
         <img  className = "burlaMaskImg" src="burlamask.png" width="120vh" height="120vh"/>
         <h1>BurlaMask</h1>
       </div>
@@ -14,6 +14,30 @@ function App() {
         </form>
       </div>
     </header>
+
+    <div className="background">
+      <img src="background.png" width="1000vh" height="500vh"/>
+    </div>
+    <div className="background">
+      <img src="background.png" width="1000vh" height="500vh"/>
+    </div>
+    
+    <footer>
+      <div className="wrapFooter">
+        <img  className = "burlaMaskImg" src="burlamask.png" width="120vh" height="120vh"/>
+      </div>
+      <p>BurlaMask</p>
+      
+      <div className="team">
+        <h2>Team</h2>
+        <ul>
+          <li>Tishik Vitalii</li>
+          <li>Tkachenko Danil</li>
+          <li>Kiriyak Nazarii</li>
+          <li>Pan Stepan</li>
+        </ul>
+      </div>
+    </footer>
     </div>
   );
 }
