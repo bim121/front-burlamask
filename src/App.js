24ec1d56a5fdbbc68d75cdde7fe7b80cfa2da93d
bim121 <vitisik54@gmail.com>
2023-06-03 10:33:51 +0300

    dashboard

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import MainPage from './MainPage/MainPage';
 import Login from './FormComponents/LoginPage/Login';
 import Register from './FormComponents/RegisterPage/Register';
+import DashBoard from './Dashboard/Dashboard';
 
 function App() {
   const [user, setUser] = useState();
@@ -13,6 +14,7 @@ function App() {
       <>
         <Routes>
           <Route path="/" element={<Register />}/>
+          <Route path="/dashboard" element={<DashBoard />}/>
           <Route path="/login" element= {<Login setUser={setUser} setRole = {setRole}/>}/>
           <Route path="/mainpage" element= {<MainPage user={user} setUser={setUser} role = {role} setRole = {setRole}/>}/>
         </Routes>
