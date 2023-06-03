4b23fc73135e2e0918ea9deba58d360a43ccc7fa
bim121 <vitisik54@gmail.com>
2023-06-03 03:32:22 +0300

    fix

--- src/ModalComponent/imageModalComponent/ImageModalComponent.js
+++ src/ModalComponent/imageModalComponent/ImageModalComponent.js
@@ -13,16 +13,6 @@ const ImageModalComponent = ({ isOpen, closeModal, data}) => {
       <div>
           <h1>Photo Details</h1>
           <div className={styles.info}>
-<<<<<<< HEAD
-          <img src={data} alt={`Photo`} className={styles.modalImage}/>
-          <div className={styles.modalDescription}><p className={styles.descriptionText}>Description:</p>{data.description}</div>
-          </div>
-          <div className={styles.buttons}>
-            <button onClick={closeModal} className={styles.buttonModalDownload}>Download</button>
-            <button onClick={closeModal} className={styles.buttonModalDelete}>Delete</button> 
-            <button onClick={closeModal} className={styles.buttonModalClose}>Close</button>
-          </div>
-=======
             <img src={data.image.url} alt={`Photo`} className={styles.modalImage}/>
             <div className={styles.modalDescription}> 
               <p className={styles.descriptionText}>
@@ -32,7 +22,6 @@ const ImageModalComponent = ({ isOpen, closeModal, data}) => {
             </div>
           </div> 
           <button onClick={closeModal} className={styles.buttonModalClose}>Close</button>
->>>>>>> c9ce64679b09daa1fe9ab8677ed580fd52dca9f6
       </div>
     </Modal>
   );
